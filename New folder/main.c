41ee66ff9a99d1391f0a5dffd1a7b5b7acf81fd2
prem1512b <rs4224177@gmail.com>
2026-04-03 00:17:36 +0530

    Initial Commit

---
+++ New folder/main.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+
+int main() {
+    int n, i, isPrime = 1;
+
+    printf("Enter number: ");
+    scanf("%d", &n);
+
+    if (n <= 1)
+        isPrime = 0;
+
+    for (i = 2; i <= n/2; i++) {
+        if (n % i == 0) {
+            isPrime = 0;
+            break;
+        }
+    }
+
+    if (isPrime)
+        printf("Prime number");
+    else
+        printf("Not prime");
+
+    return 0;
+}
